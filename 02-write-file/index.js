f5f5bd90feb95ad339c787529afd8280f98d89da
iozefavichus <iozefavichus@gmail.com>
2023-05-03 21:31:48 +0300

    feat: add solution for 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,1 +1,21 @@
+const fs = require('fs');
+const path = require('node:path');
+const { stdin, stdout, exit } = require('process');
+const absPath = path.join(__dirname,'text.txt');
+const output = fs.createWriteStream(absPath);
+
+stdout.write('Enter text, please \n');
+stdin.on('data', data => {
+  if (data.toString().trim() === 'exit') {
+    FunctionExit();
+  }
+  output.write(data);
+});
+
+process.on('SIGINT', FunctionExit);
+
+function FunctionExit() {
+  stdout.write('Bye!');
+  exit();
+}
 
