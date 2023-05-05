5fff870a0791ebaffded1e04490a2a5642c7394c
iozefavichus <iozefavichus@gmail.com>
2023-05-05 10:49:58 +0300

    feat: add solution for 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,22 @@
+const fs = require('node:fs');
+const fsPromises = fs.promises;
+const path = require('node:path');
+const styles = path.join(__dirname, 'styles');
+const dist = path.join(__dirname, 'project-dist/bundle.css');
+const output = fs.createWriteStream(dist);
+
+fsPromises
+  .readdir(styles)
+  .then(async (files) => {
+    files.forEach(async (file) => {
+      const filePath = path.join(styles, file);
+      const fileName = path.basename(filePath);
+      const ext = path.extname(filePath);
+      if (ext === '.css') {
+        const input = fs.createReadStream(path.join(styles, fileName));
+        input.on('data', data => {
+          output.write(data.toString() + '\n');
+        });
+      }
+     });
+  });
